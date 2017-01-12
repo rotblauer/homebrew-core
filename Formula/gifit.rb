f422f1d3000bab0753915d1396a21e32cba06fd3
Mr. Is <isaac.ardis@gmail.com>
2017-01-12 12:33:51 +0100

    gifit 0.1 (new formula)

---
+++ Formula/gifit.rb
@@ -0,0 +1,37 @@
+# Documentation: http://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Gifit < Formula
+  desc "Get a random gif from Giphy in markdown format in stdout."
+  homepage "https://github.com/rotblauer/gifit"
+  url "https://github.com/rotblauer/gifit/archive/v.0.1.tar.gz"
+  sha256 "087508d31ae1fbe545a4d04646a9048ad1c808e8a004303d3da22e9ee2791e2d"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test gifit`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
